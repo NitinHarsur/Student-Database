b0fc2c796a5ee54d8ea38d0827c9baf313f87572
Nitin Harsur <nitinharsur@gmail.com>
2024-02-04 12:23:59 +0530

    mongodb successfully connected

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,32 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const express = require('express');
-const app =express();
+const app = express();
 
-app.get('/',(req,res)=>{
+const DB = 'mongodb+srv://nitinharsur:mongo%40123@cluster0.zjmwqdw.mongodb.net/studentdb?retryWrites=true&w=majority';
+
+mongoose.connect(DB).then(() => {
+    console.log("Connection successful");
+}).catch((err) => {
+    console.error("Connection failed:");
+});
+
+app.get('/', (req, res) => {
     res.send('hello');
 });
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.send('About');
 });
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     res.send('Login');
 });
 
-app.get('/contact',(req,res)=>{
+app.get('/contact', (req, res) => {
     res.send('Contact');
 });
-app.listen(3000, ()=>{
+
+app.listen(3000, () => {
     console.log("done");
-})
+});
