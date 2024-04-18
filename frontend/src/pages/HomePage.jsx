7c472d142fe6371e34e0dd0860bf98d344fc627b
sidduastagi <siddu141204@gmail.com>
2024-04-18 18:13:32 +0530

    updataed homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Navbar from '../NavAndFoot/Navbar';
-import { Link } from 'react-router-dom';
 import LMS from '../images/lms1.png';
 import SSP from '../images/ssp1.png';
+import WhatsAppIcon from '../images/whatsapp.png'
+import InstagramIcon from '../images/instagram.png'
+import FacebookIcon from '../images/facebook.png'
 import './Homepage.css';
+
+
 
 function HomePage() {
   return (
@@ -14,7 +18,7 @@ function HomePage() {
           <section className="image-section">
 
             <div className="image-wrapper">
-              
+
               <a href="https://karnatakalms.com/" target="_blank" rel="noopener noreferrer">
                 <img src={LMS} alt="LMS" />
               </a>
@@ -27,11 +31,24 @@ function HomePage() {
           </section>
         </main>
         <footer>
-          <p>Student Database Management System - Powered by React</p>
+        <div className="social-icons">
+            <a href="..whatsapp" target="_blank" rel="noopener noreferrer">
+              <img src={WhatsAppIcon} alt="WhatsApp" />
+            </a>
+            <a href="..instagram" target="_blank" rel="noopener noreferrer">
+              <img src={InstagramIcon} alt="Instagram" />
+            </a>
+            <a href=".. facebook" target="_blank" rel="noopener noreferrer">
+              <img src={FacebookIcon} alt="Facebook" />
+            </a>
+          </div>
+          
         </footer>
+        
+  </div>
       </div>
-    </div>
+    
   );
 }
 
-export default HomePage;
+export default HomePage
