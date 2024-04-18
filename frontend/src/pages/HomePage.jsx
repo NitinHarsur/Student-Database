729b15714e3c6c2da5fade67d00303a79740e7e5
sidduastagi <siddu141204@gmail.com>
2024-04-18 15:25:37 +0530

    homepage updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import Navbar from '../NavAndFoot/Navbar'
+import { Link } from 'react-router-dom'
+import LMS from '../images/lms1.png'
+import SSP from '../images/ssp1.png'
+import './Homepage.css'
 
 function HomePage() {
   return (
     
     <div>
-      <Navbar/>this is HomePage</div>
+      <Navbar/>
+      <div className="homepage">
+      <main>
+        <section className="image-section">
+          <div className="image-wrapper">
+            <Link to="https://karnatakalms.com/">
+              <img src={LMS} />
+            </Link>
+            <Link to="https://ssp.postmatric.karnataka.gov.in/">
+              <img src={SSP} />
+            </Link>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <p>Student Database Management System - Powered by React</p>
+      </footer></div> </div>
   )
 }
 
